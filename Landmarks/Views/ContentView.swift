1f35bcb92866319c32d926f3b65a529f1d46fde5
dieantu <whoami@dieantu.com>
2022-07-21 00:01:08 +0200

    Tutorial by Apple

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -6,16 +6,19 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
+
+
